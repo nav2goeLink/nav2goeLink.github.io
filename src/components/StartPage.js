a3a517c169bf3133297ea162a2251bbfc596dba9
Kaiuby <kai.hoering@hotmail.de>
2021-08-07 13:28:36 +0200

    Build Update

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -7,9 +7,11 @@ const StartPage = (props) => {
   // ClickHandler
   const [hideStartForm, setStartFormHidden] = useState(false);
   const [callSearchPage, setCallSearchPage] = useState(false);
+  const [hideIntroSentences, setHideIntroSentences] = useState(false);
 
   const continueClickedHandler = () => {
     setCallSearchPage(true);
+    setHideIntroSentences(true);
   };
 
   return (
@@ -21,44 +23,54 @@ const StartPage = (props) => {
             <h1 className="logo">Nav2Gö</h1>
             <div id="welcomeText">
               <p>
-                <br></br>
-                Wilkommen. <br></br>
-                Nutzen sie diese App um sich in folgender Situation
-                weiterzuhelfen:
-                <br></br>
-                <br></br>Sie haben sich dazu entschieden, Ihre Freunde in
-                Göttingen zu besuchen. Angekommen am Hauptbahnhof Göttingen 
-                wollen Sie die öffentlichen Verkehrsmittel nutzen, um zu Ihnen
-                weiterfahren.
-                <br></br>
-                <br></br>
-                Ihre Freunde haben Ihnen bereits mitgeteilt, dass Sie an der
-                Haltestelle "Treuenhagen" wohnen. Da sie Ihre Freunde nun
-                unglücklicherweise nicht mehr erreichen können, haben Sie sich
-                die lokale Mobilitäts-App „Nav2Gö“ heruntergeladen, um sich vor
-                Ort zurechtzufinden. <br></br> <br></br>Nutzen Sie für die
-                Verbindungssuche vom Hauptbahnhof nach Treuenhagen das aktuelle
-                Datum und die aktuelle Uhrzeit.<br></br>
+                {hideIntroSentences ? null : (
+                  <>
+                    <br></br>
+                    Wilkommen. <br></br>
+                    Nutzen sie diese App um sich in folgender Situation
+                    weiterzuhelfen:
+                    <br></br>
+                    <br></br>Sie haben sich dazu entschieden, Ihre Freunde in
+                    Göttingen zu besuchen. Nachdem Sie am Hauptbahnhof in
+                    Göttingen angekommen sind, wollen Sie die öffentlichen
+                    Verkehrsmittel nutzen, um zu ihren Freunden zu fahren.
+                    <br></br>
+                    <br></br>
+                    Ihre Freunde haben Ihnen bereits mitgeteilt, dass Sie an der
+                    Haltestelle "Treuenhagen" wohnen. Da sie Ihre Freunde nun
+                    unglücklicherweise nicht mehr erreichen können, haben Sie
+                    sich die lokale Mobilitäts-App „Nav2Gö“ heruntergeladen, um
+                    sich vor Ort zurechtzufinden. <br></br> <br></br>
+                  </>
+                )}
+                Nutzen Sie für die Verbindungssuche vom Hauptbahnhof nach
+                Treuenhagen das aktuelle Datum und die aktuelle Uhrzeit.
                 <br></br>
               </p>
             </div>
-            <h1 id="appVersionAnzeige2" >App-Version 2</h1>
+            <h1 id="appVersionAnzeige2">App-Version 2</h1>
           </header>
-          </form>
-          )}
+        </form>
+      )}
 
-          {callSearchPage ? (
-            <SearchPage onGoBack={() => setCallSearchPage(false)} onSetStartFormHidden ={setStartFormHidden} />
-          ) : (
-            <button
-              id="welcomeTextButton"
-              onClick={continueClickedHandler}
-              type="submit"
-            >
-              Weiter
-            </button>
-          )}
-          {/* {clicked ? ( <SearchPage /> ) : (
+      {callSearchPage ? (
+        <SearchPage
+          onGoBack={() => {
+            setCallSearchPage(false);
+            setHideIntroSentences(false);
+          }}
+          onSetStartFormHidden={setStartFormHidden}
+        />
+      ) : (
+        <button
+          id="welcomeTextButton"
+          onClick={continueClickedHandler}
+          type="submit"
+        >
+          Weiter
+        </button>
+      )}
+      {/* {clicked ? ( <SearchPage /> ) : (
             <button className='button' onClick={()=> {continueClickedHandler(); setHideStartDisplay(true);  }} type="submit">
               Weiter
             </button>)
