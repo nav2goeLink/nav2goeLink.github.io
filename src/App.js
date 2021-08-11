ee5f807fc93b07adeab65dabb66f439cdbeac017
InkaDrum <katharinagirndt@yahoo.de>
2021-08-11 11:27:55 +0200

    build 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { useState } from "react";
-//import Verbindung from "./components/Verbindung.js";
-//import ConnectionDisplay from "./components/ConnectionDisplay.js";
-import StartPage from "./components/StartPage.js";
+import LinkPage from "./LinkPage.js";
 
-import SearchPage from "./components/SearchPage.js";
-import Verbindungsanzeige from "./components/ConnectionDisplay.js";
-import End from "./components/End";
-//import  from "./components/Verbindungsauswahl.js";
-import ConnectionDisplay from "./components/ConnectionDisplay.js";
-
-const App = () => {
+function App() {
   return (
-    // diese <> </> Umschließung funktioniert in diesem Projekt als Ersatz für einen Wrapper oder <React.Fragment/>
-    <>
-      {/* <Test/> */}
-      <StartPage />
-      {/* <Startseite /> */}
-      {/* <Verbindung />; */}
-    </>
+    <div>
+      <LinkPage />
+    </div>
   );
-};
+}
 
 export default App;
---
+++ src/LinkPage.js
@@ -0,0 +1,66 @@
+import "./Style.css";
+
+const LinkPage = () => {
+  const matchLink = () => {
+    let linknumber = Math.floor(Math.random() * 4);
+    if (linknumber === 0) {
+      return "https://morza95.github.io";
+    }
+    if (linknumber === 1) {
+      return "https://kaiuby.github.io";
+    }
+    if (linknumber === 2) {
+      return "https://nav2goekatha2.github.io";
+    }
+    if (linknumber === 3) {
+      return "https://nav2goe4.github.io";
+    }
+  };
+
+  return (
+    <div id="containerStartButtonPage">
+      <div>
+        <p id="UseCaseDescribtion">
+          <br></br>
+          Sehr geehrte(r) Teilnehmer(in),
+          <br></br>
+          <br></br> im Rahmen unserer Forschungsarbeit an der
+          Georg-August-Universität Göttingen beschäftigen wir uns mit dem Thema
+          "Designing apps: A comparative evaluation of the importance of
+          functionality, performance and visual appeal" und bitten Sie, zwei
+          Web-Apps zu testen und an einer Befragung teilzunehmen. Die Teilnahme
+          dauert insgesamt ca. 5-10 Minuten.
+          <br></br>
+          <br></br> Bitte lesen Sie die Beschreibung in den Apps sorgfältig
+          durch.
+          <br></br>
+          <br></br> Vielen Dank für die Teilnahme!
+          <br></br>
+          <br></br> Bei Fragen und Anmerkungen zu dieser Studie wenden Sie sich
+          gerne an:
+          <br></br>
+          <br></br>franz.buschendorf@stud.uni-goettingen.de
+          <br></br>katharina.girndt@stud.uni-goettingen.de
+          <br></br>kai.hoering@stud.uni-goettingen.de
+          <br></br>
+          <br></br>
+          {/* {(e) => {
+      e.preventDefault();
+      window.location.href='http://google.com';
+      }}
+
+window.location.href="http://url.com";
+
+onClick(){
+    window.location.href="http://url.com";
+} */}
+        </p>
+        <a class="buttonStartButton" href={matchLink()}>
+          Start Nav2Gö
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default LinkPage;
